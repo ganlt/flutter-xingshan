555479bafd38d21bd032c55118ddc146c03e3a20
jinshuilinxi <2329135071@qq.com>
2019-11-06 14:01:04 +0800

    add project page and found page

--- lib/page/found/found_page.dart
+++ lib/page/found/found_page.dart
@@ -12,18 +12,93 @@ class XSFoundPage extends StatefulWidget {
 
 class _XSFoundPageState extends State<XSFoundPage> {
 
+   _renderItem(image, text, index) {
+    var borderWidth = index == 1 || index == 3 ? 9.0 : 3.0;
+
+    return new Container(
+      padding: EdgeInsets.fromLTRB(2.0, 10.0, 0, 10.0),
+      constraints: BoxConstraints.tightFor(width: double.infinity),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: XSColors.primaryLineValue,
+            width: borderWidth,
+            style: BorderStyle.solid,
+          ),
+        ),
+      ),
+      child: FlatButton(
+        child: Flex(
+          direction: Axis.horizontal,
+          children: <Widget>[
+            Expanded(
+              flex: 15,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 14.0),
+                    child: new Image(
+                      image: new AssetImage(image),
+                      // color: XSColors.primarySwatch,
+                    ),
+                  ),
+                  Text(
+                    text,
+                    textScaleFactor: 1.2,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  new Image(
+                    image: new AssetImage(XSICons.MY_EXPAND),
+                  )
+                ],
+              ),
+            ),
+            Spacer(
+              flex: 1,
+            )
+          ],
+        ),
+        onPressed: () {
+          // NavigatorUtils.goMyPage(index);
+        },
+      ),
+    );
+  }
+
   // final EdgeInsets _padding = MediaQuery.of(context).padding;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          constraints: BoxConstraints.tightFor(height: 164.0),
-          decoration: BoxDecoration(
-            color: XSColors.primarySwatch
+    
+    List<Widget> _items = [
+      _renderItem(XSICons.FOUND_TASK, '任务', 1),
+      _renderItem(XSICons.FOUND_DYNAMIC, '动态', 2),
+      _renderItem(XSICons.FOUND_TEAM, '团队', 3),
+      _renderItem(XSICons.FOUND_SQUARE, '广场', 4),
+    ];
+
+    return Scaffold(
+      appBar: new AppBar(
+        title: Center(
+          child: Text(
+            '发现',
+            style: TextStyle(
+              color: XSColors.white,
+            ),
           ),
-        ),
-      ],
+        )
+      ),
+      body: ListView(
+        children: _items
+      )
     );
   }
 }
--- lib/page/project/project_page.dart
+++ lib/page/project/project_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:xingshan/common/style/style.dart';
 
+import 'package:xingshan/common/utils/navigator_utils.dart';
 class XSProjectPage extends StatefulWidget {
   XSProjectPage({Key key, this.title}): super(key: key);
 
@@ -12,18 +15,230 @@ class XSProjectPage extends StatefulWidget {
 
 class _XSProjectPageState extends State<XSProjectPage> {
 
+  _renderItem(image, title, current, other, target, des) {
+    return FlatButton(
+      // highlightElevation: 
+      child: Container(
+        padding: EdgeInsets.fromLTRB(10.0, 7.0, 10.0, 7.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Image(
+                image: new AssetImage(image),
+                // width: 345,
+                // height: 194,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    '[${title}]',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    des,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: SizedBox(
+                height: 10,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  child: LinearProgressIndicator(
+                    backgroundColor: XSColors.primaryLineValue,
+                    valueColor: AlwaysStoppedAnimation(XSColors.primaryValue),
+                    value: .3, 
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Column(
+                children: <Widget>[
+                  Flex(
+                    direction: Axis.horizontal,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  current,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: XSColors.primarySwatch
+                                  ),
+                                  textAlign: TextAlign.left,
+                                )
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Center(
+                                  child: Text(
+                                    '捐步筹款',
+                                    style: TextStyle(
+                                      color: XSColors.primaryTabValue,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: <Widget>[
+                            Center(
+                              child: Text(
+                                other,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: XSColors.primarySwatch
+                                ),
+                              ),
+                            ),
+                            Center(
+                              child: Text(
+                                '其他筹款',
+                                  style: TextStyle(
+                                    color: XSColors.primaryTabValue,
+                                      fontSize: 12,
+                                  ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  target,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: XSColors.primarySwatch
+                                  ),
+                                )
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Center(
+                                  child: Text(
+                                    '目标筹款',
+                                    style: TextStyle(
+                                      color: XSColors.primaryTabValue,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      onPressed: () {
+        // NavigatorUtils.goProject();
+      },
+    );
+  }
   // final EdgeInsets _padding = MediaQuery.of(context).padding;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          constraints: BoxConstraints.tightFor(height: 164.0),
-          decoration: BoxDecoration(
-            color: XSColors.primarySwatch
-          ),
+    List<Widget> _items = [
+      _renderItem(
+        'assets/images/project.png',
+        '免费午餐',
+        '4500元',
+        '6500元',
+        '100万元',
+        '为山区孩子筹集午餐，大家一起来支持吧！'
+      ),
+      _renderItem(
+        'assets/images/project_02.png',
+        '免费午餐',
+        '6.98万',
+        '13.02万',
+        '20万元',
+        '为山区孩子筹集午餐，大家一起来支持吧！'
+      ),
+      _renderItem(
+        'assets/images/project.png',
+        '免费午餐',
+        '4500元',
+        '6500元',
+        '100万元',
+        '为山区孩子筹集午餐，大家一起来支持吧！'
+      ),
+      _renderItem(
+        'assets/images/project.png',
+        '免费午餐',
+        '4500元',
+        '6500元',
+        '100万元',
+        '为山区孩子筹集午餐，大家一起来支持吧！'
+      )
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            '待支付的项目',
+            style: TextStyle(
+              color: XSColors.white,
+            ),
+          )
         ),
-      ],
+      ),
+      body: Container(
+        // color: Theme.of(context).primaryColor,
+        child: ListView(
+          children: _items,
+        ),
+      ),
     );
   }
 }
