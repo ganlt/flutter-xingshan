713773bc2a354799a257429506e9e4f8789836d4
jinshuilinxi <2329135071@qq.com>
2019-11-06 13:59:43 +0800

    add common utils

--- lib/common/utils/common_utils.dart
+++ lib/common/utils/common_utils.dart
@@ -48,4 +48,59 @@ class CommonUtils {
   static XSStringBase getLocale(BuildContext context) {
     return XSLocalizations.of(context).currentLocalized;
   }
+  static showLanguageDialog(BuildContext context, Store store) {
+    List<String> list = [
+      CommonUtils.getLocale(context).home_language_default,
+      CommonUtils.getLocale(context).home_language_zh,
+      CommonUtils.getLocale(context).home_language_en,
+    ];
+    // CommonUtils.showCommitOptionDialog(context, list, (index) {
+    //   CommonUtils.changeLocale(store, index);
+    //   LocalStorage.save(Config.LOCALE, index.toString());
+    // }, height: 150.0);
+  }
+  // static Future<Null> showCommitOptionDialog(
+  //   BuildContext context,
+  //   List<String> commitMaps,
+  //   ValueChanged<int> onTap, {
+  //   width = 250.0,
+  //   height = 400.0,
+  //   List<Color> colorList,
+  // }) {
+  //   return NavigatorUtils.showGSYDialog(
+  //       context: context,
+  //       builder: (BuildContext context) {
+  //         return Center(
+  //           child: new Container(
+  //             width: width,
+  //             height: height,
+  //             padding: new EdgeInsets.all(4.0),
+  //             margin: new EdgeInsets.all(20.0),
+  //             decoration: new BoxDecoration(
+  //               color: GSYColors.white,
+  //               //用一个BoxDecoration装饰器提供背景图片
+  //               borderRadius: BorderRadius.all(Radius.circular(4.0)),
+  //             ),
+  //             child: new ListView.builder(
+  //                 itemCount: commitMaps.length,
+  //                 itemBuilder: (context, index) {
+  //                   return GSYFlexButton(
+  //                     maxLines: 1,
+  //                     mainAxisAlignment: MainAxisAlignment.start,
+  //                     fontSize: 14.0,
+  //                     color: colorList != null
+  //                         ? colorList[index]
+  //                         : Theme.of(context).primaryColor,
+  //                     text: commitMaps[index],
+  //                     textColor: GSYColors.white,
+  //                     onPress: () {
+  //                       Navigator.pop(context);
+  //                       onTap(index);
+  //                     },
+  //                   );
+  //                 }),
+  //           ),
+  //         );
+  //       });
+  // }
 }
